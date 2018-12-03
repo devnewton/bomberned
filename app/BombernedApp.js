a58d7cf79ebcfde84cf91f57a31abaf621f39936
devnewton <devnewton@bci.im>
2018-12-03 22:08:16 +0100

    remove js files

--- app/BombernedApp.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const BombernedGame_1 = require("./BombernedGame");
-new BombernedGame_1.BombernedGame();
-//# sourceMappingURL=BombernedApp.js.map
